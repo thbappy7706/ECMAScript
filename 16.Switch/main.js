6f7d6fbbe7076e7963c51a5be3023a64bbf9c2ed
Tanvir Hossen Bappy <tanvir15-7706@diu.edu.bd>
2020-11-09 03:09:08 +0600

    Switch Case

---
+++ 16.Switch/main.js
@@ -0,0 +1,67 @@
+// let digit = prompt("Enter a number");
+
+// switch(digit){
+//     case "0":
+//         document.write('The Number is Zero');
+//         break;
+    
+//         case "1":
+//             document.write('The Number is One');
+//             break;
+       
+//         case "2":
+//             document.write('The Number is Two');
+//             break;
+    
+//         case "3":
+//             document.write('The Number is Three');
+//             break;   
+            
+//         case "5":
+//             document.write('The Number is Five');
+//             break;
+        
+//         case "6":
+//             document.write('The Number is Six');
+//             break; 
+            
+//         case "7":
+//             document.write('The Number is Seven');
+//             break;
+
+//         case "8":
+//             document.write('The Number is Eight');
+//             break;
+
+//             case "9":
+//                 document.write('The Number is Nine');
+//                 break;
+        
+
+//                 default:
+//                     document.write('Not a digit');
+// }
+
+
+
+
+//check vowel consonent
+
+
+let letter = prompt("Enter a letter to check");
+letter = letter.toLowerCase();
+
+
+switch(letter){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+        case "u":
+            document.write("Its a vowel");
+            break;
+
+            default:
+                document.write("Its a Consonent");
+        
+}
