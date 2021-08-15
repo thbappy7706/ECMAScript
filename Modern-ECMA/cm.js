a90d10a38f4c1eab2ede72b6a7ce4583209bcf41
thbappy7706 <tanvir15-7706@diu.edu.bd>
2021-08-15 10:23:53 +0600

    cm

---
+++ Modern-ECMA/cm.js
@@ -0,0 +1,30 @@
+task.plugin('buble', {}, function * (file, opts) {
+    opts = Object.assign({
+        inline: false,
+        sourceMap: true,
+        file: file.base,
+        source: relPath(file)
+    }, opts);
+
+    // Transform data
+    const output = transform(file.data.toString(), opts);
+
+    // Handle sourcemaps
+    if (opts.sourceMap) {
+        if (opts.inline) {
+            // Append inline sourcemap
+            output.code += `\n//# sourceMappingURL=${output.map.toUrl()}`;
+        } else {
+            output.code += `\n//# sourceMappingURL=${opts.file}.map`;
+            // Create new file
+            this._.files.push({
+                base: `${opts.file}.map`,
+                data: output.map.toString(),
+                dir: file.dir
+            });
+        }
+    }
+
+    // doc: Return contents as Buffer!
+    file.data = new Buffer(output.code);
+});
